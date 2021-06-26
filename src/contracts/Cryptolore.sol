bc911263d6987a9036ef930df1ae49e15a3f01d3
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-06-26 15:30:44 +0530

    Added smart contract

---
+++ src/contracts/Cryptolore.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.5.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Cryptolore is ERC721 {
+  constructor() ERC721("Cryptolore", "CPL") {}
+
+  uint public worksId;
+
+  mapping(uint=>Work) public works;
+
+  struct Work {
+    uint id;
+    uint256 price;
+    string title;
+    string content;
+    address payable writer;
+    address owner;
+    bool isSold;
+  }
+
+  function addWork(uint256 _price, string memory _title, string memory _content) public {
+    require(_price>0);
+    worksId++;
+    works[worksId] = Work(worksId, _price, _title, _content, msg.sender, msg.sender, false);
+  }
+}
